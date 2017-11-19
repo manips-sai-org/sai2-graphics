b977e5a24b5fac1e65e672c50f765c5be0038020
Mikael Jorda <mjorda@stanford.edu>
2017-11-18 20:36:41 -0800

    added two simple examples

---
+++ examples/02-update_rendering/main.cpp
@@ -0,0 +1,125 @@
+// This example application loads a URDF world file and simulates two robots
+// with physics and contact in a Dynamics3D virtual world. A graphics model of it is also shown using 
+// Chai3D.
+
+#include "Sai2Graphics.h"
+
+#include <GLFW/glfw3.h> //must be loaded after loading opengl/glew
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const string world_file = "resources/world.urdf";
+const string robot_file = "resources/rbot.urdf";
+const string robot_name = "RBot";
+const string camera_name = "camera_fixed";
+
+// callback to print glfw errors
+void glfwError(int error, const char* description);
+
+// callback when a key is pressed
+void keySelect(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+int main() {
+	cout << "Loading URDF world model file: " << world_file << endl;
+
+	// load graphics scene
+	auto graphics = new Sai2Graphics::Sai2Graphics(world_file, true);
+
+    // load robot
+    auto robot = new Sai2Model::Sai2Model(robot_file, false);
+    int dof = robot->dof();
+    robot->_q.setZero();
+    robot->_dq.setZero();
+
+	/*------- Set up visualization -------*/
+    // set up error callback
+    glfwSetErrorCallback(glfwError);
+
+    // initialize GLFW
+    glfwInit();
+
+    // retrieve resolution of computer display and position window accordingly
+    GLFWmonitor* primary = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(primary);
+
+    // information about computer screen and GLUT display window
+	int screenW = mode->width;
+    int screenH = mode->height;
+    int windowW = 0.8 * screenH;
+    int windowH = 0.5 * screenH;
+    int windowPosY = (screenH - windowH) / 2;
+    int windowPosX = windowPosY;
+
+    // create window and make it current
+    glfwWindowHint(GLFW_VISIBLE, 0);
+    GLFWwindow* window = glfwCreateWindow(windowW, windowH, "01-parse_world_and_robot", NULL, NULL);
+	glfwSetWindowPos(window, windowPosX, windowPosY);
+	glfwShowWindow(window);
+    glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+    // set callbacks
+	glfwSetKeyCallback(window, keySelect);
+
+    unsigned long long counter = 0;
+
+    // while window is open:
+    while (!glfwWindowShouldClose(window))
+	{
+        // update robot position
+        robot->_q << (double) counter/100.0;
+        robot->updateKinematics();
+
+		// update graphics. this automatically waits for the correct amount of time
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+        graphics->updateGraphics(robot_name, robot);
+		graphics->render(camera_name, width, height);
+
+		// swap buffers
+		glfwSwapBuffers(window);
+
+		// wait until all GL commands are completed
+		glFinish();
+
+		// check for any OpenGL errors
+		GLenum err;
+		err = glGetError();
+		assert(err == GL_NO_ERROR);
+
+	    // poll for events
+	    glfwPollEvents();
+
+        counter++;
+	}
+
+    // destroy context
+    glfwDestroyWindow(window);
+
+    // terminate
+    glfwTerminate();
+
+	return 0;
+}
+
+//------------------------------------------------------------------------------
+
+void glfwError(int error, const char* description) {
+	cerr << "GLFW Error: " << description << endl;
+	exit(1);
+}
+
+//------------------------------------------------------------------------------
+
+void keySelect(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    // option ESC: exit
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        // exit application
+         glfwSetWindowShouldClose(window, 1);
+    }
+}
