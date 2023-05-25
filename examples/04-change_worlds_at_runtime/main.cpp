cff555cb1a77de90771501d736a0607ae36426c0
Mikael <mjorda@stanford.edu>
2023-05-25 17:01:46 +0200

    updated example with new api

--- examples/04-change_worlds_at_runtime/main.cpp
+++ examples/04-change_worlds_at_runtime/main.cpp
@@ -18,17 +18,14 @@ const string camera_name = "camera_fixed";
 
 int main() {
     // load graphics scene
-    auto graphics = new Sai2Graphics::Sai2Graphics(world_file_1, true);
-    graphics->initializeWindow();
+    auto graphics = new Sai2Graphics::Sai2Graphics(world_file_1);
 
     // load robot
     auto robot1 = new Sai2Model::Sai2Model(robot_file, false);
-    robot1->_q.setZero();
-    robot1->_dq.setZero();
-
     auto robot2 = new Sai2Model::Sai2Model(robot_file, false);
-    robot2->_q.setZero();
-    robot2->_dq.setZero();
+
+    Eigen::VectorXd q_robot1 = Eigen::VectorXd::Zero(robot1->dof());
+    Eigen::VectorXd q_robot2 = Eigen::VectorXd::Zero(robot2->dof());
 
     unsigned long long counter = 0;
     int current_loaded_world = 0;
@@ -36,34 +33,33 @@ int main() {
     // while window is open:
     while (graphics->isWindowOpen()) {
         // update robot position
-        robot1->_q[0] += 0.01;
+        q_robot1(0) += 0.01;
+        robot1->set_q(q_robot1);
         robot1->updateKinematics();
 
         // update graphics rendering and window contents
         graphics->updateGraphics(robot_name_1, robot1);
         if(current_loaded_world == 1) {
-            robot2->_q[0] -= 0.01;
+            q_robot2(0) -= 0.01;
+            robot2->set_q(q_robot2);
             robot2->updateKinematics();
             graphics->updateGraphics(robot_name_2, robot2);
         }
-        graphics->render(camera_name);
-        graphics->updateWindowWithCameraView(camera_name);
+        graphics->updateDisplayedWorld(camera_name);
 
         if(counter % 700 == 350) {
             graphics->resetWorld(world_file_2);
-            robot1->_q[0] = 0;
-            robot2->_q[0] = 0;
+            q_robot1.setZero();
+            q_robot2.setZero();
             current_loaded_world = 1 - current_loaded_world;
         }
         if((counter % 700 == 0) && (counter != 0)) {
             graphics->resetWorld(world_file_1);
-            robot1->_q[0] = 0;
+            q_robot1.setZero();
             current_loaded_world = 1 - current_loaded_world;
         }
         counter++;
     }
-
-    graphics->closeWindow();
 
     return 0;
 }
